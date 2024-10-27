62f524164e30e935d2c696b522cf896cb672f15d
Sam Johnston <samj@samj.net>
2024-10-27 09:35:18 -0700

    categories displaying

--- wordpress/plugin/licenses/includes/patterns.php
+++ wordpress/plugin/licenses/includes/patterns.php
@@ -8,6 +8,16 @@ function register_license_block_patterns() {
         array('label' => __('Licenses', 'open-source-licenses'))
     );
 
+    // Add new categories pattern
+    register_block_pattern(
+        'licenses/categories',
+        array(
+            'title'       => __('License Categories', 'open-source-licenses'),
+            'categories'  => array('licenses'),
+            'content'     => get_license_categories_pattern(),
+        )
+    );
+
     register_block_pattern(
         'licenses/table-view',
         array(
@@ -47,17 +57,18 @@ function get_license_table_pattern() {
     $rows = '';
     foreach ($licenses as $license) {
         $title = get_the_title($license);
-        // Remove ellipsis and just do a clean word trim
         $excerpt = wp_trim_words(get_the_excerpt($license), 50, '');
         $categories = get_the_terms($license->ID, 'license_category');
         $category_names = $categories ? join(', ', wp_list_pluck($categories, 'name')) : '';
         
         $rows .= <<<HTML
+        <!-- wp:tablerow -->
         <tr>
             <td class="has-text-align-left"><a href="{$license->guid}">{$title}</a></td>
             <td class="has-text-align-left">{$excerpt}</td>
             <td class="has-text-align-left">{$category_names}</td>
         </tr>
+        <!-- /wp:tablerow -->
         HTML;
     }
 
@@ -67,6 +78,7 @@ function get_license_table_pattern() {
     <!-- wp:table {"className":"is-style-regular license-table","fontSize":"small"} -->
     <figure class="wp-block-table is-style-regular license-table has-small-font-size">
         <table>
+            <!-- wp:table-head -->
             <thead>
                 <tr>
                     <th class="has-text-align-left" style="width:20%">License</th>
@@ -74,9 +86,12 @@ function get_license_table_pattern() {
                     <th class="has-text-align-left" style="width:20%">Category</th>
                 </tr>
             </thead>
+            <!-- /wp:table-head -->
+            <!-- wp:table-body -->
             <tbody>
                 {$rows}
             </tbody>
+            <!-- /wp:table-body -->
         </table>
     </figure>
     <!-- /wp:table -->
@@ -102,9 +117,15 @@ function get_license_grid_pattern() {
         $cards .= <<<HTML
         <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|30","right":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30"}},"border":{"width":"1px"}},"backgroundColor":"background","layout":{"type":"flex","orientation":"vertical"}} -->
         <div class="wp-block-group has-background-background-color has-background" style="border-width:1px;padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)">
+            <!-- wp:heading {"level":3} -->
             <h3 class="wp-block-heading"><a href="{$license->guid}">{$title}</a></h3>
-            <div class="license-excerpt">{$excerpt}</div>
-            <div class="license-categories">{$category_names}</div>
+            <!-- /wp:heading -->
+            <!-- wp:paragraph {"className":"license-excerpt"} -->
+            <p class="license-excerpt">{$excerpt}</p>
+            <!-- /wp:paragraph -->
+            <!-- wp:paragraph {"className":"license-categories"} -->
+            <p class="license-categories">{$category_names}</p>
+            <!-- /wp:paragraph -->
         </div>
         <!-- /wp:group -->
         HTML;
@@ -131,24 +152,55 @@ function get_license_list_pattern() {
     return <<<HTML
 <!-- wp:group {"layout":{"type":"constrained"}} -->
 <div class="wp-block-group">
-    <!-- wp:query {"queryId":1,"query":{"postType":"license","perPage":250}} -->
+    <!-- wp:query {"queryId":1,"query":{"postType":"license","perPage":250,"inherit":false}} -->
     <div class="wp-block-query">
-        <!-- wp:post-template -->
-            <!-- wp:group {"className":"license-list-item","layout":{"type":"flex","flexWrap":"nowrap"}} -->
-            <div class="wp-block-group license-list-item">
-                <!-- wp:post-title {"level":3,"isLink":true} /-->
-                <!-- wp:post-excerpt {"showMoreOnNewLine":false,"excerptLength":20} /-->
-            </div>
-            <!-- /wp:group -->
+        <!-- wp:post-template {"layout":{"type":"default"}} -->
+        <!-- wp:group {"className":"license-list-item","layout":{"type":"flex","flexWrap":"nowrap"}} -->
+        <div class="wp-block-group license-list-item">
+            <!-- wp:post-title {"level":3,"isLink":true} /-->
+            <!-- wp:post-excerpt {"showMoreOnNewLine":false,"excerptLength":20} /-->
+        </div>
+        <!-- /wp:group -->
         <!-- /wp:post-template -->
 
-        <!-- wp:query-pagination -->
-            <!-- wp:query-pagination-previous /-->
-            <!-- wp:query-pagination-numbers /-->
-            <!-- wp:query-pagination-next /-->
+        <!-- wp:query-pagination {"layout":{"type":"flex","justifyContent":"center"}} -->
+        <!-- wp:query-pagination-previous /-->
+        <!-- wp:query-pagination-numbers /-->
+        <!-- wp:query-pagination-next /-->
         <!-- /wp:query-pagination -->
     </div>
     <!-- /wp:query -->
+</div>
+<!-- /wp:group -->
+HTML;
+}
+
+// Add new function for categories pattern
+function get_license_categories_pattern() {
+    $categories = get_terms(array(
+        'taxonomy' => 'license_category',
+        'hide_empty' => true,
+    ));
+    
+    $category_items = '';
+    foreach ($categories as $category) {
+        $category_items .= <<<HTML
+        <!-- wp:button {"className":"license-category-button"} -->
+        <div class="wp-block-button license-category-button">
+            <a href="?license_category={$category->slug}" class="wp-block-button__link wp-element-button">{$category->name}</a>
+        </div>
+        <!-- /wp:button -->
+        HTML;
+    }
+
+    return <<<HTML
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group">
+    <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center","orientation":"horizontal","flexWrap":"wrap"}} -->
+    <div class="wp-block-buttons">
+        {$category_items}
+    </div>
+    <!-- /wp:buttons -->
 </div>
 <!-- /wp:group -->
 HTML;
