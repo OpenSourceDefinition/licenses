b6df0ed16756d8973c7a2c451daaedc9e3939948
Sam Johnston <samj@samj.net>
2024-10-27 08:49:40 -0700

    fix grid

--- wordpress/plugin/licenses/includes/patterns.php
+++ wordpress/plugin/licenses/includes/patterns.php
@@ -38,73 +38,90 @@ function register_license_block_patterns() {
 add_action('init', 'register_license_block_patterns');
 
 function get_license_table_pattern() {
+    $args = array(
+        'post_type' => 'license',
+        'posts_per_page' => 250,
+    );
+    $licenses = get_posts($args);
+    
+    $rows = '';
+    foreach ($licenses as $license) {
+        $title = get_the_title($license);
+        // Remove ellipsis and just do a clean word trim
+        $excerpt = wp_trim_words(get_the_excerpt($license), 50, '');
+        $categories = get_the_terms($license->ID, 'license_category');
+        $category_names = $categories ? join(', ', wp_list_pluck($categories, 'name')) : '';
+        
+        $rows .= <<<HTML
+        <tr>
+            <td class="has-text-align-left"><a href="{$license->guid}">{$title}</a></td>
+            <td class="has-text-align-left">{$excerpt}</td>
+            <td class="has-text-align-left">{$category_names}</td>
+        </tr>
+        HTML;
+    }
+
     return <<<HTML
 <!-- wp:group {"layout":{"type":"constrained"}} -->
 <div class="wp-block-group">
-    <!-- wp:query {"queryId":1,"query":{"postType":"license","perPage":250}} -->
-    <div class="wp-block-query">
-        <!-- wp:table {"className":"license-table"} -->
-        <figure class="wp-block-table license-table">
-            <table>
-                <thead>
-                    <tr>
-                        <th>License</th>
-                        <th>Description</th>
-                        <th>Type</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <!-- wp:post-template -->
-                        <tr>
-                            <td><!-- wp:post-title {"level":0,"isLink":true} /--></td>
-                            <td><!-- wp:post-excerpt {"showMoreOnNewLine":false} /--></td>
-                            <td><!-- wp:post-terms {"term":"category"} /--></td>
-                        </tr>
-                    <!-- /wp:post-template -->
-                </tbody>
-            </table>
-        </figure>
-        <!-- /wp:table -->
-
-        <!-- wp:query-pagination -->
-            <!-- wp:query-pagination-previous /-->
-            <!-- wp:query-pagination-numbers /-->
-            <!-- wp:query-pagination-next /-->
-        <!-- /wp:query-pagination -->
-    </div>
-    <!-- /wp:query -->
+    <!-- wp:table {"className":"is-style-regular license-table","fontSize":"small"} -->
+    <figure class="wp-block-table is-style-regular license-table has-small-font-size">
+        <table>
+            <thead>
+                <tr>
+                    <th class="has-text-align-left" style="width:20%">License</th>
+                    <th class="has-text-align-left" style="width:60%">Description</th>
+                    <th class="has-text-align-left" style="width:20%">Category</th>
+                </tr>
+            </thead>
+            <tbody>
+                {$rows}
+            </tbody>
+        </table>
+    </figure>
+    <!-- /wp:table -->
 </div>
 <!-- /wp:group -->
 HTML;
 }
 
 function get_license_grid_pattern() {
+    $args = array(
+        'post_type' => 'license',
+        'posts_per_page' => 250,
+    );
+    $licenses = get_posts($args);
+    
+    $cards = '';
+    foreach ($licenses as $license) {
+        $title = get_the_title($license);
+        $excerpt = wp_trim_words(get_the_excerpt($license), 25, '');
+        $categories = get_the_terms($license->ID, 'license_category');
+        $category_names = $categories ? join(', ', wp_list_pluck($categories, 'name')) : '';
+        
+        $cards .= <<<HTML
+        <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|30","right":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30"}},"border":{"width":"1px"}},"backgroundColor":"background","layout":{"type":"flex","orientation":"vertical"}} -->
+        <div class="wp-block-group has-background-background-color has-background" style="border-width:1px;padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)">
+            <h3 class="wp-block-heading"><a href="{$license->guid}">{$title}</a></h3>
+            <div class="license-excerpt">{$excerpt}</div>
+            <div class="license-categories">{$category_names}</div>
+        </div>
+        <!-- /wp:group -->
+        HTML;
+    }
+
     return <<<HTML
 <!-- wp:group {"layout":{"type":"constrained"}} -->
 <div class="wp-block-group">
     <!-- wp:heading {"level":1,"className":"page-title"} -->
-    <h1 class="page-title">Open Source Licenses</h1>
+    <h1 class="wp-block-heading page-title">Open Source Licenses</h1>
     <!-- /wp:heading -->
 
-    <!-- wp:query {"queryId":1,"query":{"postType":"license","perPage":250}} -->
-    <div class="wp-block-query">
-        <!-- wp:post-template {"layout":{"type":"grid","columnCount":3}} -->
-            <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|30","right":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30"}},"border":{"width":"1px"}},"backgroundColor":"background","layout":{"type":"flex","orientation":"vertical"}} -->
-            <div class="wp-block-group">
-                <!-- wp:post-title {"level":2,"isLink":true} /-->
-                <!-- wp:post-excerpt {"showMoreOnNewLine":false} /-->
-                <!-- wp:post-terms {"term":"license_type"} /-->
-            </div>
-            <!-- /wp:group -->
-        <!-- /wp:post-template -->
-
-        <!-- wp:query-pagination -->
-            <!-- wp:query-pagination-previous /-->
-            <!-- wp:query-pagination-numbers /-->
-            <!-- wp:query-pagination-next /-->
-        <!-- /wp:query-pagination -->
+    <!-- wp:group {"className":"license-grid","layout":{"type":"grid","minimumColumnWidth":"300px"}} -->
+    <div class="wp-block-group license-grid">
+        {$cards}
     </div>
-    <!-- /wp:query -->
+    <!-- /wp:group -->
 </div>
 <!-- /wp:group -->
 HTML;
